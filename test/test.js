fd1840c59079fc6134264656ff32f24308bcff47
Derrick Kirwa <dkip64@gmail.com>
2019-08-01 05:57:33 +0300

    refactor vote automation

--- test/test.js
+++ test/test.js
@@ -1,52 +1,54 @@
 const { Builder, By } = require('selenium-webdriver');
 const fs = require('fs');
 
-const letsVote = async () => {
-  let driver = await new Builder().forBrowser('chrome').build();
+// set target vote here
+let targetVotes = 100;
 
-  describe('Vote', async () => {
+const letsVote = async () => {
+  describe('Vote automation', async () => {
+    let driver = new Builder().forBrowser('chrome').build();
     await driver.get(
-      'https://myleader.co.ke/mlk-officials-countrywide-voting/'
+      'https://www.opinionstage.com/polls/2559731/poll?sembed=1&wid=1'
     );
-    await driver.findElement(By.xpath("//a[@href='#health']")).click();
-    let iframeSwitch = driver.findElement(
-      By.xpath(
-        "//iframe[@src='https://www.opinionstage.com/polls/2559731/poll?sembed=1&wid=2']"
-      )
-    );
-    await driver.switchTo().frame(iframeSwitch);
     await driver
       .findElement(By.xpath("//label[contains(text(),'Okoth Onyango')]"))
       .click();
-
-    await driver.findElement(
-      By.xpath(
-        '//*[@id="os-body"]/div[1]/div[3]/div/div[3]/div[1]/div[2]/div/a'
-      ).click()
-    );
+    await driver
+      .findElement(
+        By.xpath(
+          '//*[@id="os-body"]/div[1]/div[3]/div/div[3]/div[1]/div[2]/div/a'
+        )
+      )
+      .click();
     await driver.quit();
   });
 };
+
+let count = 0;
+const interval = setInterval(async () => {
+  count += 1;
+  let currentTime = new Date();
+  if (count === targetVotes) {
+    clearInterval(interval);
+    await resultsScreenshot();
+    console.log(`Finished, candidate is ${count - 1} votes ahead :)`);
+    return;
+  }
+  let time = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
+  console.log(`Casting vote ${count} at ${time}`);
+  letsVote();
+}, 5000);
+
 const resultsScreenshot = async () => {
-  await driver.get('https://myleader.co.ke/mlk-officials-countrywide-voting/');
+  let driver = new Builder().forBrowser('chrome').build();
+  await driver.get(
+    'https://www.opinionstage.com/polls/2559731/poll?sembed=1&wid=1'
+  );
   await driver.takeScreenshot().then(data => {
     let base64Data = data.replace(/^data:image\/png;base64,/, '');
     fs.writeFile('screenshot.png', base64Data, 'base64', err => {
       if (err) console.log(err);
     });
   });
+  await driver.quit();
 };
-
-let startTime = new Date().getTime();
-const interval = setInterval(async () => {
-  let currentTime = new Date();
-  if (currentTime.getTime() - startTime > 600000) {
-    clearInterval(interval);
-    await resultsScreenshot();
-    console.log('Finished, candidate is 9 votes ahead :)');
-    return;
-  }
-  let time = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
-  console.log(`Casting one vote at ${time}`);
-  letsVote();
-}, 60000);
