829f6d9020266d3b51be354b4af4998e68764421
Derrick Kirwa <dkip64@gmail.com>
2020-01-09 01:52:02 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const hi = () => {
-  var now = new Date();
-  var time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-  console.log('Hello', time);
-};
-setInterval(() => {
-  hi();
-}, 5000);
